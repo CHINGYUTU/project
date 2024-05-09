b507b9c07c471f0acc9c3af4c571ed4921323cac
CHINGYU <11236034@ntub.edu.tw>
2024-05-09 20:43:26 +0800

    113.05.09Newpost

--- work/map/map/urls.py
+++ work/map/map/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from mapapp.views import map
+from mapapp.views import index,map
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index',index),
     path('map',map),
 ]
--- work/map/mapapp/views.py
+++ work/map/mapapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request,"index.html",locals())
 def map(requset):
     return render(requset,"map.html",locals())
