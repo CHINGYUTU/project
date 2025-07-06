abb79c9ba8b542d15beff38f7b153f1af830c556
CHINGYU <11236034@ntub.edu.tw>
2025-07-06 14:42:42 +0800

    114.07.06 data collation & upload UI

--- work/map/map/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from mapapp.views import index,map
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index',index),
-    path('map',map),
-]
